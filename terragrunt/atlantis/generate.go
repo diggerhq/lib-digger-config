364dbc11431c1f478aecae1b29d33cf907ab283f
motatoes <moe.habib9@gmail.com>
2023-10-23 20:06:05 +0100

    fix tests

--- terragrunt/atlantis/generate.go
+++ terragrunt/atlantis/generate.go
@@ -664,7 +664,7 @@ func Parse(gitRoot string, projectHclFiles []string, createHclProjectExternalChi
 	// Ensure the gitRoot has a trailing slash and is an absolute path
 	absoluteGitRoot, err := filepath.Abs(gitRoot)
 	if err != nil {
-		fmt.Print("failed to fetch git root: %v", err)
+		fmt.Printf("failed to fetch git root: %v", err)
 		return nil, nil, err
 	}
 	gitRoot = absoluteGitRoot + string(filepath.Separator)
@@ -699,7 +699,7 @@ func Parse(gitRoot string, projectHclFiles []string, createHclProjectExternalChi
 	for _, workingDir := range workingDirs {
 		terragruntFiles, err := getAllTerragruntFiles(filterPath, projectHclFiles, workingDir)
 		if err != nil {
-			fmt.Print("failed to get terragrunt files of %v: %v", workingDir, err)
+			fmt.Printf("failed to get terragrunt files of %v: %v", workingDir, err)
 			return nil, nil, err
 		}
 
@@ -782,7 +782,7 @@ func Parse(gitRoot string, projectHclFiles []string, createHclProjectExternalChi
 			projectHcl := lookupProjectHcl(projectHclDirMap, workingDir)
 			err := sem.Acquire(ctx, 1)
 			if err != nil {
-				fmt.Print("failed to acquire lock: %v", err)
+				fmt.Printf("failed to acquire lock: %v", err)
 				return nil, nil, err
 			}
 
@@ -807,7 +807,7 @@ func Parse(gitRoot string, projectHclFiles []string, createHclProjectExternalChi
 			})
 
 			if err := errGroup.Wait(); err != nil {
-				fmt.Print("failed while group wait: %v", err)
+				fmt.Printf("failed while group wait: %v", err)
 				return nil, nil, err
 			}
 		}
